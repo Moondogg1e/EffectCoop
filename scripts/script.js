8403ad808608c687e7b1b5d6916c15ad230d40d7
KholodnyiMaksym <maks.holod.kiev@gmail.com>
2021-03-18 00:43:41 +0200

    hotfix for slider

--- scripts/script.js
+++ scripts/script.js
@@ -20,7 +20,7 @@ if (slider !== null) {
 	function mobileSlider() {
 		if (window.innerWidth <= 1100 && slider.dataset.mobile == 'false') {
 			mySwiper = new Swiper(slider, {
-				slidesPerView: 'auto',
+				slidesPerView: 2,
 				// loop: true,
 				centeredSlides: true,
 				loop: true,
